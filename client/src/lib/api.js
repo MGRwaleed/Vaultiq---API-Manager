fc6f3d603ca12c05a5063a9a89dca0dc130465cb
MGRwaleed <waleeddastagir1@gmail.com>
2026-06-01 15:40:32 +0530

    Few Changes

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: 'https://vaultiq-76sz.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
